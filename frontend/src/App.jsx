94dd8b706ac07b424be30d90edefaa58f895cd03
deepvishwakarma.2114@gmail.com <deepvishwakarma.2114@gmail.com>
2026-06-09 11:35:46 +0530

    Added create ticket form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,13 @@ import axios from "axios";
 function App() {
   const [tickets, setTickets] = useState([]);
 
+  const [formData, setFormData] = useState({
+    customer_name: "",
+    customer_email: "",
+    subject: "",
+    description: "",
+  });
+
   useEffect(() => {
     fetchTickets();
   }, []);
@@ -20,9 +27,90 @@ function App() {
     }
   };
 
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const createTicket = async () => {
+    try {
+      await axios.post(
+        "http://127.0.0.1:8000/api/tickets",
+        formData
+      );
+
+      setFormData({
+        customer_name: "",
+        customer_email: "",
+        subject: "",
+        description: "",
+      });
+
+      fetchTickets();
+
+      alert("Ticket Created Successfully!");
+    } catch (error) {
+      console.log(error);
+      alert("Error creating ticket");
+    }
+  };
+
   return (
     <div style={{ padding: "20px", fontFamily: "Arial" }}>
       <h1>Customer Support CRM</h1>
+
+      <hr />
+
+      <h2>Create Ticket</h2>
+
+      <input
+        type="text"
+        name="customer_name"
+        placeholder="Customer Name"
+        value={formData.customer_name}
+        onChange={handleChange}
+      />
+
+      <br />
+      <br />
+
+      <input
+        type="email"
+        name="customer_email"
+        placeholder="Email"
+        value={formData.customer_email}
+        onChange={handleChange}
+      />
+
+      <br />
+      <br />
+
+      <input
+        type="text"
+        name="subject"
+        placeholder="Subject"
+        value={formData.subject}
+        onChange={handleChange}
+      />
+
+      <br />
+      <br />
+
+      <textarea
+        name="description"
+        placeholder="Description"
+        value={formData.description}
+        onChange={handleChange}
+      />
+
+      <br />
+      <br />
+
+      <button onClick={createTicket}>
+        Create Ticket
+      </button>
 
       <hr />
 
